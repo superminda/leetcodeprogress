9f039e9d2749acb36ef452fabdb40ee840b1f7cc
superminda <mf3308@nyu.edu>
2025-10-22 00:40:51 -0400

    Create 2585-DeleteGreatestValueInEachRow

---
+++ 2585-DeleteGreatestValueInEachRow/2585-DeleteGreatestValueInEachRow.java
@@ -0,0 +1,20 @@
+// Last updated: 10/22/2025, 12:40:50 AM
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        int result = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(grid[i]);
+        }
+        
+        for (int j = 0; j < n; j++) {
+            int max = 0;
+            for (int i = 0; i < m; i++) {
+                max = Math.max(max, grid[i][j]);
+            }
+            result += max;
+        }
+        return result;
+    }
+}
