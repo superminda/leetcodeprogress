b61dcb2fce21246ec9067dac497db8b6e2b7372b
superminda <mf3308@nyu.edu>
2025-10-22 00:40:37 -0400

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.java
@@ -0,0 +1,9 @@
+// Last updated: 10/22/2025, 12:40:36 AM
+class Solution {
+    public int distinctIntegers(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n - 1;
+    }
+}
