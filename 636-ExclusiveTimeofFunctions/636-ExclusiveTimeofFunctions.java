fb8b67a158c6599d4560bee13a837bf79eb9805f
superminda <mf3308@nyu.edu>
2025-10-26 17:13:47 -0400

    File updated at 10/26/2025, 5:13:47 PM

--- 636-ExclusiveTimeofFunctions/636-ExclusiveTimeofFunctions.java
+++ 636-ExclusiveTimeofFunctions/636-ExclusiveTimeofFunctions.java
@@ -1,36 +1,23 @@
-// Last updated: 10/26/2025, 2:45:58 PM
-class ExecutionDetail {
-    public int id;
-    public int start;
-    public int delay;
-    public ExecutionDetail(int id, int start) {
-        this.id = id;
-        this.start = start;
-        this.delay = 0;
-    }
-}
-
+// Last updated: 10/26/2025, 5:13:47 PM
 class Solution {
-    public int[] exclusiveTime(int n, List<String> logs) {
-        int[] result = new int[n];
-        Stack<ExecutionDetail> stack = new Stack<>();
-        for (String log : logs) {
-            String[] parts = log.split(":");
-            int thisId = Integer.valueOf(parts[0]);
-            boolean isStart = parts[1].equals("start");
-            int timestamp = Integer.valueOf(parts[2]);
-            if (isStart) {
-                stack.push(new ExecutionDetail(thisId, timestamp));
-            } else {
-                ExecutionDetail thisExecution = stack.pop();
-                int executionTime = timestamp - thisExecution.start + 1 - thisExecution.delay;
-                result[thisId] += executionTime;
-                if (!stack.isEmpty()) {
-                    int overallLatency = timestamp - thisExecution.start + 1;
-                    stack.peek().delay += overallLatency;
-                }
+    private int result;
+    public int countSubstrings(String s) {
+        this.result = 0;
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            count(charArray, i, i);
+            if (i != 0) {
+                count(charArray, i - 1, i);
             }
         }
-        return result;
+        return this.result;
+    }
+
+    private void count(char[] charArray, int left, int right) {
+        if (left < 0 || right >= charArray.length || charArray[left] != charArray[right]) {
+            return;
+        }
+        this.result++;
+        count(charArray, left - 1, right + 1);
     }
 }
