98b6284b20cb1c524b0ee7c8a4620be9c0804004
superminda <mf3308@nyu.edu>
2025-12-01 22:52:56 -0500

    File updated at 12/1/2025, 10:52:56 PM

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,28 +1,37 @@
-// Last updated: 12/1/2025, 12:45:43 AM
-1class Solution {
-2    public int longestOnes(int[] nums, int k) {
-3        int result = 0;
-4        int n = nums.length;
-5        int left = 0;
-6        int usedCount = 0;
-7        for (int index = 0; index < n; index++) {
-8            if (nums[index] == 0) {
-9                if (k != 0) {
-10                    while (usedCount == k) {
-11                        if (nums[left] == 0) {
-12                            usedCount--;
-13                        }
-14                        left++;
-15                    }
-16                    usedCount++;
-17                    result = Math.max(result, index - left + 1);
-18                } else {
-19                    left = index + 1;
-20                }
-21            } else {
-22                result = Math.max(result, index - left + 1);
-23            }
-24        }
-25        return result;
-26    }
-27}
+// Last updated: 12/1/2025, 10:52:56 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeKLists(ListNode[] lists) {
+13        int k = lists.length;
+14        if (k == 0) {
+15            return null;
+16        }
+17        PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(k, (a, b) -> (a.val - b.val));
+18        for (ListNode list : lists) {
+19            if (list == null) {
+20                continue;
+21            }
+22            heap.offer(list);
+23        }
+24        ListNode dummyNode = new ListNode();
+25        ListNode currentNode = dummyNode;
+26        while (!heap.isEmpty()) {
+27            ListNode pollNode = heap.poll();
+28            currentNode.next = pollNode;
+29            currentNode = pollNode;
+30            if (pollNode.next != null) {
+31                heap.offer(pollNode.next);
+32            }
+33        }
+34        return dummyNode.next;
+35    }
+36}
