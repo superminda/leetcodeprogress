57c4403724fc5062e782badc94a1b064a55c6516
superminda <mf3308@nyu.edu>
2025-12-03 00:40:48 -0500

    File updated at 12/3/2025, 12:40:48 AM

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,82 +1,34 @@
-// Last updated: 12/2/2025, 1:37:18 AM
+// Last updated: 12/3/2025, 12:40:48 AM
 1class Solution {
-2    private static int[] xMoves = {-1, 0, 1, 0};
-3    private static int[] yMoves = {0, 1, 0, -1};
-4    public int largestIsland(int[][] grid) {
-5        int result = 0;
-6        int n = grid.length;
-7        int[][] groups = new int[n][n];
-8        for (int i = 0; i < n; i++) {
-9            for (int j = 0; j < n; j++) {
-10                if (grid[i][j] == 1) {
-11                    groups[i][j] = i * n + j + 1;
-12                }
+2
+3    private static char DELIMILTER = ' ';
+4    private static String MA = "ma";
+5    private static String A = "a";
+6    public String toGoatLatin(String sentence) {
+7        String[] strs = sentence.split("\\s+");
+8        StringBuilder sb = new StringBuilder();
+9        for (int index = 0; index < strs.length; index++) {
+10            char[] charArray = strs[index].toCharArray();
+11            if (index != 0) {
+12                sb.append(DELIMILTER);
 13            }
-14        }
-15        for (int i = 0; i < n; i++) {
-16            for (int j = 0; j < n; j++) {
-17                if (grid[i][j] == 0) {
-18                    continue;
+14            if (isVowel(charArray[0])) {
+15                sb.append(strs[index]);
+16            } else {
+17                for (int i = 1; i < charArray.length; i++) {
+18                    sb.append(charArray[i]);
 19                }
-20                for (int k = 0; k < xMoves.length; k++) {
-21                    int newX = i + xMoves[k];
-22                    int newY = j + yMoves[k];
-23                    if (newX < 0 || newX >= n || newY < 0 || newY >= n || grid[newX][newY] == 0) {
-24                        continue;
-25                    }
-26                    union(groups, i, j, newX, newY);
-27                }
-28            }
-29        }
-30        int[] count = new int[n * n + 1];
-31        for (int i = 0; i < n; i++) {
-32            for (int j = 0; j < n; j++) {
-33                if (grid[i][j] == 0) {
-34                    continue;
-35                }
-36                int thisGroup = findGroup(groups, i, j);
-37                count[thisGroup]++;
-38                result = Math.max(result, count[thisGroup]);
-39            }
-40        }
-41        for (int i = 0; i < n; i++) {
-42            for (int j = 0; j < n; j++) {
-43                if (grid[i][j] == 1) {
-44                    continue;
-45                }
-46                int thisCount = 1;
-47                Set<Integer> groupSet = new HashSet<>();
-48                for (int k = 0; k < xMoves.length; k++) {
-49                    int newX = i + xMoves[k];
-50                    int newY = j + yMoves[k];
-51                    if (newX < 0 || newX >= n || newY < 0 || newY >= n || grid[newX][newY] == 0) {
-52                        continue;
-53                    }
-54                    int thisGroup = findGroup(groups, newX, newY);
-55                    if (groupSet.add(thisGroup)) {
-56                        thisCount += count[thisGroup];
-57                    }
-58                }
-59                result = Math.max(result, thisCount);
-60            }
-61        }
-62        return result;
-63    }
-64    
-65    private void union(int[][] groups, int x1, int y1, int x2, int y2) {
-66        int group1 = findGroup(groups, x1, y1);
-67        int group2 = findGroup(groups, x2, y2);
-68        int group1X = (group1 - 1) / groups.length;
-69        int group1Y = (group1 - 1) % groups.length;
-70        groups[group1X][group1Y] = group2;
-71    }
-72
-73    private int findGroup(int[][] groups, int x, int y) {
-74        if ((groups[x][y] - 1) / groups.length == x && y == (groups[x][y] - 1) % groups.length) {
-75            return groups[x][y];
-76        }
-77        int fatherGroup = findGroup(groups, (groups[x][y] - 1) / groups.length, (groups[x][y] - 1) % groups.length);
-78        groups[x][y] = fatherGroup;
-79        return fatherGroup;
-80    }
-81}
+20                sb.append(charArray[0]);
+21            }
+22            sb.append(MA);
+23            for (int i = 0; i <= index; i++) {
+24                sb.append(A);
+25            }
+26        }
+27        return sb.toString();
+28    }
+29
+30    private boolean isVowel(char c) {
+31        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+32    }
+33}
