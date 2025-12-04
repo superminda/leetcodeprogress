a10014f214dc4eac0787dc36c53ffbaf2708fc96
superminda <mf3308@nyu.edu>
2025-12-04 00:46:55 -0500

    File updated at 12/4/2025, 12:46:55 AM

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,27 +1,54 @@
-// Last updated: 12/4/2025, 12:25:27 AM
-1class Solution {
-2    public List<List<Integer>> subsets(int[] nums) {
-3        List<List<Integer>> result = new ArrayList<>();
-4        List<Integer> subResult = new ArrayList<>();
-5        List<Integer> emptyResult = new ArrayList<>();
-6        result.add(emptyResult);
-7        for (int i = 0; i < nums.length; i++) {
-8            dfs(nums, i, result, subResult);
-9        }
-10        return result;
-11    }
-12
-13    private void dfs(int[] nums, int index, List<List<Integer>> result, List<Integer> subResult) {
-14        if (index == nums.length) {
-15            return;
-16        }
-17        subResult.add(nums[index]);
-18        List<Integer> newSubResult = new ArrayList<>();
-19        newSubResult.addAll(subResult);
-20        result.add(newSubResult);
-21        for (int i = index + 1; i < nums.length; i++) {
-22            dfs(nums, i, result, subResult);
-23        }
-24        subResult.remove(subResult.size() - 1);
-25    }
-26}
+// Last updated: 12/4/2025, 12:46:55 AM
+1class Bank {
+2
+3    private long[] accounts;
+4    public Bank(long[] balance) {
+5        this.accounts = balance;
+6    }
+7    
+8    public boolean transfer(int account1, int account2, long money) {
+9        int index1 = account1 - 1;
+10        if (index1 < 0 || index1 >= this.accounts.length) {
+11            return false;
+12        }
+13        if (money > this.accounts[index1]) {
+14            return false;
+15        }
+16        int index2 = account2 - 1;
+17        if (index2 < 0 || index2 >= this.accounts.length) {
+18            return false;
+19        }
+20        this.accounts[index1] -= money;
+21        this.accounts[index2] += money;
+22        return true;
+23    }
+24    
+25    public boolean deposit(int account, long money) {
+26        int index = account - 1;
+27        if (index < 0 || index >= this.accounts.length) {
+28            return false;
+29        }
+30        this.accounts[index] += money;
+31        return true;
+32    }
+33    
+34    public boolean withdraw(int account, long money) {
+35        int index = account - 1;
+36        if (index < 0 || index >= this.accounts.length) {
+37            return false;
+38        }
+39        if (money > this.accounts[index]) {
+40            return false;
+41        }
+42        this.accounts[index] -= money;
+43        return true;
+44    }
+45}
+46
+47/**
+48 * Your Bank object will be instantiated and called as such:
+49 * Bank obj = new Bank(balance);
+50 * boolean param_1 = obj.transfer(account1,account2,money);
+51 * boolean param_2 = obj.deposit(account,money);
+52 * boolean param_3 = obj.withdraw(account,money);
+53 */
