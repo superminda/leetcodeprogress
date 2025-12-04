bddc6b6d089e2f78f689062dcfaff3d6f99b362c
superminda <mf3308@nyu.edu>
2025-12-04 01:49:03 -0500

    File updated at 12/4/2025, 1:49:03 AM

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,54 +1,59 @@
-// Last updated: 12/4/2025, 12:46:55 AM
-1class Bank {
-2
-3    private long[] accounts;
-4    public Bank(long[] balance) {
-5        this.accounts = balance;
-6    }
-7    
-8    public boolean transfer(int account1, int account2, long money) {
-9        int index1 = account1 - 1;
-10        if (index1 < 0 || index1 >= this.accounts.length) {
-11            return false;
-12        }
-13        if (money > this.accounts[index1]) {
-14            return false;
-15        }
-16        int index2 = account2 - 1;
-17        if (index2 < 0 || index2 >= this.accounts.length) {
-18            return false;
-19        }
-20        this.accounts[index1] -= money;
-21        this.accounts[index2] += money;
-22        return true;
-23    }
-24    
-25    public boolean deposit(int account, long money) {
-26        int index = account - 1;
-27        if (index < 0 || index >= this.accounts.length) {
-28            return false;
-29        }
-30        this.accounts[index] += money;
-31        return true;
-32    }
-33    
-34    public boolean withdraw(int account, long money) {
-35        int index = account - 1;
-36        if (index < 0 || index >= this.accounts.length) {
-37            return false;
-38        }
-39        if (money > this.accounts[index]) {
-40            return false;
-41        }
-42        this.accounts[index] -= money;
-43        return true;
-44    }
-45}
-46
-47/**
-48 * Your Bank object will be instantiated and called as such:
-49 * Bank obj = new Bank(balance);
-50 * boolean param_1 = obj.transfer(account1,account2,money);
-51 * boolean param_2 = obj.deposit(account,money);
-52 * boolean param_3 = obj.withdraw(account,money);
-53 */
+// Last updated: 12/4/2025, 1:49:03 AM
+1class Solution {
+2    public int threeSumClosest(int[] nums, int target) {
+3        int closestSum = (int)1E7;
+4        Arrays.sort(nums);
+5        for (int i = 0; i < nums.length - 2; i++) {
+6            for (int j = i + 1; j < nums.length - 1; j++) {
+7                int targetNum = target - nums[i] - nums[j];
+8                int index = findBiggestNumberIndexSmallerOrEqual(nums, targetNum, j + 1);
+9                if (index == -1) {
+10                    int sumDiff = Math.abs(targetNum - nums[j + 1]);
+11                    if (sumDiff < Math.abs(closestSum - target)) {
+12                        closestSum = nums[i] + nums[j] + nums[j + 1];
+13                    }
+14                } else if (nums[index] == targetNum) {
+15                    return target;
+16                } else if (nums[index] > targetNum) {
+17                    int sumDiff = Math.abs(targetNum - nums[index]);
+18                    if (sumDiff < Math.abs(closestSum - target)) {
+19                        closestSum = nums[i] + nums[j] + nums[index];
+20                    }
+21                } else {
+22                    int sumDiff = Math.abs(targetNum - nums[index]);
+23                    if (sumDiff < Math.abs(closestSum - target)) {
+24                        closestSum = nums[i] + nums[j] + nums[index];
+25                    }
+26                    if (index + 1 <= nums.length - 1) {
+27                        sumDiff = Math.abs(targetNum - nums[index + 1]);
+28                        if (sumDiff < Math.abs(closestSum - target)) {
+29                            closestSum = nums[i] + nums[j] + nums[index + 1];
+30                        }
+31                    }
+32                }
+33            }
+34        }
+35        return closestSum;
+36    }
+37
+38    private int findBiggestNumberIndexSmallerOrEqual(int[] nums, int targetNum, int startIndex) {
+39        int left = startIndex;
+40        int right = nums.length - 1;
+41        while (left + 1 < right) {
+42            int mid = left + (right - left) / 2;
+43            int midNum = nums[mid];
+44            if (midNum <= targetNum) {
+45                left = mid;
+46            } else {
+47                right = mid - 1;
+48            }
+49        }
+50        if (nums[right] <= targetNum) {
+51            return right;
+52        } else if (nums[left] <= targetNum) {
+53            return left;
+54        } else {
+55            return -1;
+56        }
+57    }
+58}
